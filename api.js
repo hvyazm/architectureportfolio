bd09fd50031d47fe19b5cddfcf03d184b4206825
hvyazm <115558476+hvyazm@users.noreply.github.com>
2023-09-18 22:35:33 +0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,24 @@
+
+// Select the "About" link
+const aboutLink = document.querySelector('a[href="#about"]');
+
+// Add a click event listener to the "About" link
+aboutLink.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default link behavior
+    
+    // Get the target section using the hash fragment in the link
+    const targetSection = document.querySelector(event.target.getAttribute('href'));
+
+    // Scroll smoothly to the target section
+    targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start' // Scroll to the top of the section
+    });
+});
+
+$(document).ready(function(){
+    $("#mobile-menu").click(function(){
+      $("#navbar .nav-list").toggleClass("active");
+      $("#mobile-menu").toggleClass("active");
+    });
+  });
